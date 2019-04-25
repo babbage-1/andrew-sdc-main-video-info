6c072c03b727b1259e223285ae6aad8b42199892
ahchung23 <hahneol@gmail.com>
2019-04-25 02:59:21 -0400

    create new db connection to postgres and write new read query

--- db/index.js
+++ db/index.js
@@ -2,3 +2,32 @@ const { Pool } = require('pg');
 const path = require('path');
 const { config } = require('../postgres_config');
 const pool = new Pool(config);
+
+
+const getMovieInfo = async (id) => {
+  try {
+    const res = await pool.query('SELECT * FROM movieinfo WHERE id = $1', [id]);
+    console.log(`movie info for id ${id}\n`, res.rows[0]);
+    return res.rows[0];
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+// const getMoviePoster = async (id) => {
+//   try {
+//     console.time('timing query');
+//     const res = await pool.query('SELECT image FROM movieinfo WHERE id = $1', [id]);
+//     console.timeEnd('timing query');
+//     console.log(`movie poster url for id ${id}\n`, res.rows[0]);
+//     return res.rows[0];
+//   } catch (e) {
+//     console.log(e);
+//     return e;
+//   }
+// };
+
+module.exports = {
+  getMovieInfo,
+};
