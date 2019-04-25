93d2f9e106c852a6e65d44102aa8bfd696095696
ahchung23 <hahneol@gmail.com>
2019-04-25 15:44:37 -0400

    edit get request to test if id is number

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -12,7 +13,16 @@ app.use(cors());
 
 // get request for movie info
 app.get('/info/:id', async (req, res) => {
+  // assume id always given
   const movieId = req.params.id;
+  // check if id number, return error ir not
+  const isNotNumber = isNaN(Number(movieId));
+  if (isNotNumber) {
+    console.log('id not a number');
+    res.sendStatus(400);
+    return;
+  }
+
   try {
     const movieInfo = await getMovieInfo(movieId);
     // Send not found if db does not contain the movie
@@ -21,30 +31,12 @@ app.get('/info/:id', async (req, res) => {
     }
     res.json(movieInfo);
   } catch (e) {
-    res.status(500).send(e);
+    res.sendStatus(500);
   }
 });
 
-/*
-REDUNDANT API CALL. ABSOLUTELY NOT NECESSARY.
-EVEN WITHIN THE ORIGINAL REPO FROM FANDANGIT, THE API CALL MADE FOR MOVIE INFO INCLUDED THE URL LINK TO THE MOVIE POSTER AND THIS INFORMATION IS PASSED DOWN TO EVEN CHILD COMPONENTS.
-NO IDEA WHY THIS WAS INCLUDED WITHIN AS A SEPERATE API CALL.
 
-route for getting movie poster
-app.get('/info/:id/poster', (req, res) => {
-  const movieId = req.params.id;
-  db.getMoviePoster(movieId, (err, results) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      console.log(results);
-      res.json(results[0].info.image);
-    }
-  });
-});
-*/
-
-app.get('/info/:id/post', (req, res) => {
+app.post('/info/:id/update', (req, res) => {
   console.log('CREATE new item');
 
   res.status(200).send('Create New Item from POST');
@@ -66,3 +58,23 @@ app.get('/info/:id/delete', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+
+/*
+REDUNDANT API CALL. ABSOLUTELY NOT NECESSARY.
+EVEN WITHIN THE ORIGINAL REPO FROM FANDANGIT, THE API CALL MADE FOR MOVIE INFO INCLUDED THE URL LINK TO THE MOVIE POSTER AND THIS INFORMATION IS PASSED DOWN TO CHILD COMPONENTS.
+NO IDEA WHY THIS WAS INCLUDED WITHIN AS A SEPERATE API CALL.
+
+route for getting movie poster
+app.get('/info/:id/poster', (req, res) => {
+  const movieId = req.params.id;
+  db.getMoviePoster(movieId, (err, results) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      console.log(results);
+      res.json(results[0].info.image);
+    }
+  });
+});
+*/
