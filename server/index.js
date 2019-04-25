93d33ff21b2920515c71eac317e2e50d4f2877a3
ahchung23 <hahneol@gmail.com>
2019-04-25 17:27:59 -0400

    change endpoint to use handle put request with putController

--- server/index.js
+++ server/index.js
@@ -4,13 +4,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = process.env.PORT || 2000;
+
 const {
-  getMovieInfo, createMovieInfo,
-} = require('../db/index');
-const {
-  idIsNaN,
-  isDataObjDefined,
-} = require('./dataCheck');
+  readController, createController, putController,
+} = require('./controllers');
 
 app.use('/main/:id', express.static('client/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,71 +15,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // get request for movie info
-app.get('/info/:id', async (req, res) => {
-  // assume id always given
-  const movieId = req.params.id;
-  // check if id number, return error ir not
-  if (idIsNaN(movieId)) {
-    console.log('id not a number');
-    res.sendStatus(400);
-    return;
-  }
-
-  try {
-    const movieInfo = await getMovieInfo(movieId);
-    // Send not found if db does not contain the movie
-    if (movieInfo === undefined) {
-      res.sendStatus(404);
-    }
-    res.json(movieInfo);
-  } catch (e) {
-    res.sendStatus(500);
-  }
-});
+app.get('/info/:id', readController);
 
 
-app.post('/info/:id/create', async (req, res) => {
-  console.log('CREATE new item');
-  const movieId = req.params.id;
-  // return error if id not number
-  if (idIsNaN(movieId)) {
-    console.log('id not a number');
-    res.sendStatus(400);
-    return;
-  }
-  const {
-    name, genre, score, runtime, rating, releaseday, releasemonth, releaseyear, image,
-  } = req.body;
-
-  const dataObj = {
-    name, genre, score, runtime, rating, releaseday, releasemonth, releaseyear, image,
-  };
-  // return error if data obj not properly defined.
-  if (!isDataObjDefined(dataObj)) {
-    console.log('dataObj not properly defined');
-    res.sendStatus(400);
-    return;
-  }
-
-  try {
-    const createResult = await createMovieInfo(dataObj);
-    // Send not found if db does not contain the movie
-    console.log(createResult);
-    if (createResult === undefined) {
-      res.sendStatus(404);
-    }
-    res.json(createResult);
-  } catch (e) {
-    res.sendStatus(500);
-  }
-});
+app.post('/info/:id/create', createController);
 
 
-app.get('/info/:id/put', (req, res) => {
-  console.log('UPDATE old item');
-
-  res.status(200).send('Update New Item from PUT');
-});
+app.put('/info/:id/put', putController);
 
 app.get('/info/:id/delete', (req, res) => {
   console.log('DELETE new item');
