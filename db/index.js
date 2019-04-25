5cd693529a644223e4e8e2832b0bed9e65cdcef7
ahchung23 <hahneol@gmail.com>
2019-04-25 16:19:50 -0400

    use prepared query createQuery to insert new row into db

--- db/index.js
+++ db/index.js
@@ -5,14 +5,14 @@ const pool = new Pool(config);
 
 const getMovieInfo = async (id) => {
   // prepared query for faster querying
-  const getMovieInfoQuery = {
-    name: 'get-MovieInfo',
+  const getQuery = {
+    name: 'read-MovieInfo',
     text: 'SELECT * FROM movieinfo WHERE id = $1',
     values: [id],
   };
 
   try {
-    const res = await pool.query(getMovieInfoQuery);
+    const res = await pool.query(getQuery);
     console.log(`movie info for id ${id}\n`, res.rows[0]);
     return res.rows[0];
   } catch (e) {
@@ -21,31 +21,29 @@ const getMovieInfo = async (id) => {
   }
 };
 
-// const createMovieInfo = async (dataObj) => {
-//   try {
+const createMovieInfo = async (dataObj) => {
+  const {
+    name, genre, score, runtime, rating, releaseday, releasemonth, releaseyear, image,
+  } = dataObj;
 
-//     const queryString = `INSERT INTO
-//     (name, genre, score, runtime, rating, releaseday, releasemonth, releaseyear, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`
-//     const params = [];
-//     const res = await pool.query(queryString, params);
-//   } catch (e) {
-//     console.log(e);
-//     throw e;
-//   }
-// }
+  const createQuery = {
+    name: 'create-MovieInfo',
+    text: `INSERT INTO movieinfo
+    (name, genre, score, runtime, rating, releaseday, releasemonth, releaseyear, image)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+    values: [name, genre, score, runtime, rating, releaseday, releasemonth, releaseyear, image],
+  };
 
-// const getMoviePoster = async (id) => {
-//   try {
-//     console.time('timing query');
-//     const res = await pool.query('SELECT image FROM movieinfo WHERE id = $1', [id]);
-//     console.timeEnd('timing query');
-//     console.log(`movie poster url for id ${id}\n`, res.rows[0]);
-//     return res.rows[0];
-//   } catch (e) {
-//     console.log(e);
-//     return e;
-//   }
-// };
+  try {
+    const res = await pool.query(createQuery);
+    const { command, rowCount } = res;
+    console.log({ command, rowCount })
+    return { command, rowCount };
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
 
 module.exports = {
   getMovieInfo,
