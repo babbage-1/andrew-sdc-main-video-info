626c329dafa847f95ccbbbbda361ca950adfb695
ahchung23 <hahneol@gmail.com>
2019-05-02 13:45:53 -0400

    clean up comments

--- server/index.js
+++ server/index.js
@@ -25,22 +25,3 @@ const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-/*
-REDUNDANT API CALL. ABSOLUTELY NOT NECESSARY.
-EVEN WITHIN THE ORIGINAL REPO FROM FANDANGIT, THE API CALL MADE FOR MOVIE INFO INCLUDED THE URL LINK TO THE MOVIE POSTER AND THIS INFORMATION IS PASSED DOWN TO CHILD COMPONENTS.
-NO IDEA WHY THIS WAS INCLUDED WITHIN AS A SEPERATE API CALL.
-
-route for getting movie poster
-app.get('/info/:id/poster', (req, res) => {
-  const movieId = req.params.id;
-  db.getMoviePoster(movieId, (err, results) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      console.log(results);
-      res.json(results[0].info.image);
-    }
-  });
-});
-*/
